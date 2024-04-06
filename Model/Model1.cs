80b0e8d3548b20ad9a476acf728fdac044a9d11d
emper1or <emper1or38@yandex.ru>
2024-04-06 22:55:50 +0700

    Add project files.

---
+++ Model/Model1.cs
@@ -0,0 +1,89 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace Model
+{
+    public class Employee : INotifyPropertyChanged
+    {
+        private string _name;
+        private string _prepodname;
+        private string _difficulty;
+        private DateTime _date = DateTime.Today;
+ 
+        
+
+        public string Name
+        {
+            get
+
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public string PrepodName
+        {
+            get
+
+            {
+                return _prepodname;
+            }
+            set
+            {
+                _prepodname = value;
+                OnPropertyChanged("PrepodName");
+            }
+        }
+
+
+        public string Difficultu
+        {
+            get
+
+            {
+                return _difficulty;
+            }
+            set
+            {
+                _difficulty = value;
+                OnPropertyChanged("Difficultu");
+            }
+        }
+
+        public DateTime SelectedDate
+        {
+            get
+
+            {
+                return _date;
+            }
+            set
+            {
+                _date = value;
+                OnPropertyChanged("SelectedDate");
+            }
+        }
+
+        public string DateOfStartWork
+        {
+            get
+
+            {
+                return SelectedDate.AddDays(-14).ToString("M/d/yyyy").Replace('.','/');
+            }
+            set { }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ViewModel/ViemModel1.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Net.Mime;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+using Model;
+
+namespace ViewModel
+{
+    public class ViewModel1 : INotifyPropertyChanged
+    {
+        public ObservableCollection<Employee> Employees {  get; private  set; }
+        private Employee _newEmployee;
+
+        public ViewModel1()
+        {
+            Employees = new ObservableCollection<Employee>();
+            NewEmployee = new Employee();
+        }
+        public Employee NewEmployee
+        {
+            get => _newEmployee;
+            set
+            {
+                _newEmployee = value;
+                OnPropertyChanged("NewEmployee");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        private ICommand addCommand;
+        private ICommand removeCommand;
+
+        public ICommand AddCommand => addCommand ?? (addCommand = new RelayCommand(AddEmployee));
+        public ICommand RemoveCommand => removeCommand ?? (removeCommand = new RelayCommand(RemoveEmployee));
+
+        public void AddEmployee()
+        {
+            if (Employees.Contains(NewEmployee) == false)
+            {
+                if (NewEmployee.SelectedDate < DateTime.Today.AddDays(14) || NewEmployee.Difficultu == "" ||
+                    NewEmployee.Name == "" || NewEmployee.PrepodName == "")
+                {
+                }
+                else
+                {
+                    Employees.Add(NewEmployee);
+                    NewEmployee = new Employee(); }
+                
+                
+            }
+        }
+
+        public void RemoveEmployee()
+        {
+            var i_1 = -1;
+            for (int i = 0;  i < Employees.Count; i++) 
+            {
+                if (Employees[i].Name.Equals(NewEmployee.Name) & Employees[i].PrepodName.Equals(NewEmployee.PrepodName) & Employees[i].SelectedDate.Equals(NewEmployee.SelectedDate) & Employees[i].Difficultu.Equals(NewEmployee.Difficultu))
+                {
+                    i_1 = i;
+                    break;
+                }
+                
+            }
+
+            if (i_1 == -1) return;
+            Employees.RemoveAt(i_1);
+            NewEmployee = new Employee();
+
+        }
+
+
+        
+
+
+    }
+}
